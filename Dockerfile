d84f1b6b5e31c5a822f137b9064621e88367fd92
tianyawy <tianya_wy@hotmail.com>
2019-05-19 19:40:11 +0800

    temp

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM docker.elastic.co/logstash/logstash-oss:7.0.1
 RUN rm -f /usr/share/logstash/pipeline/logstash.conf
-ADD weblogic-access-pipeline.yml /usr/share/logstash/pipeline/pipelines.yml
+ADD weblogic-log-pipeline.yml /usr/share/logstash/pipeline/pipelines.yml
 VOLUME [/usr/share/var/logs]
-ENV ACCESS_LOG_PATH .//usr/share/var/logs/**/access.log
-ENV ACCESS_LOG_PATH .//usr/share/var/logs/**/access.log
+ENV ACCESS_LOG_PATH /usr/share/var/logs/**/access.log
 
 
--- install-logstash.sh
+++ install-logstash.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 env=$1
-DIRECTORY='./tmp'
-if [ -d "$DIRECTORY" ]; then
-    RM -rf $DIRECTORY 
-fi
-MKDIR $DIRECTORY
-logstashPath="$DIRECTORY/logstash.zip"
-curl https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.0.1.zip --output $logstashPath 
-unzip -o $logstashPath -d ./logstatsh/
+# DIRECTORY='./tmp'
+# if [ -d "$DIRECTORY" ]; then
+#     RM -rf $DIRECTORY 
+# fi
+# MKDIR $DIRECTORY
+# logstashPath="$DIRECTORY/logstash.zip"
+# curl https://artifacts.elastic.co/downloads/logstash/logstash-oss-7.0.1.zip --output $logstashPath 
+# unzip -o $logstashPath -d ./logstatsh/
 
 trap "" 1
-EXPORT ACCESS_LOG_PATH=${PWD}/example-access.log
-EXPORT TAGS= ["weblogic-web", "uat"]
-EXPORT ES_SERVICES=["http://localhost:9200"]
-EXPORT INDEX="claim-web-uat-%{+YYYY.MM.dd}"
-./logstatsh/logstash-7.0.0/bin/logstash -f ./weblogic-access-pipeline.yml
+export ACCESS_LOG_PATH=${PWD}/example-access.log
+export ADMIN_LOG_PATH=${PWD}/Adminserver.log
+export TAGS="[\"weblogic-web\", \"uat\"]"
+export ES_SERVICES="[\"http://localhost:9200\"]"
+export INDEX="claim-web-uat-%{+YYYY.MM.dd}"
+
+./logstatsh/logstash-7.0.1/bin/logstash -f ./weblogic-log-pipeline.yml
 
